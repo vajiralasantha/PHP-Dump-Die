3ae73c39dac3b1dc646302a992b0867eef3237a8
vajiralasantha <vajee555@gmail.com>
2019-04-09 15:38:05 +1200

    Initial project.

---
+++ src/Loader.php
@@ -0,0 +1,13 @@
+<?php
+
+use BigV\Dumper;
+
+if (!function_exists('dd')) {
+    function dd() {
+        array_map(function($x) {
+            (new Dumper)->dump($x);
+        }, func_get_args());
+
+        die(1);
+    }
+}
